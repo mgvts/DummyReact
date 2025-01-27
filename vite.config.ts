ea84205700df5bbcffb5263e54931130e7a3b45a
mrgen <mrgeniysak4795@gmail.com>
2025-01-27 17:09:34 +0300

    init

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import {fileURLToPath, URL} from 'node:url'
 
 export default defineConfig({
+    base: 'DummyReact',
     plugins: [
         react({
             include: /\.(mdx|js|jsx|ts|tsx)$/
